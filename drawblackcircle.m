55ff1b35360879603a2df0cfc58775b8e4dd6077
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2017-03-11 23:48:11 -0800

    Updates comments and formats code

--- drawblackcircle.m
+++ drawblackcircle.m
@@ -1,3 +1,6 @@
 function drawblackcircle(x,y)
+% drawblackcircle plots a black dot at an inputted x,y coordinate location
+
     plot(x,y,'o','MarkerSize',10,'color','black','MarkerFaceColor','black')
+    
 end
--- makeConnect4MoveHuman.m
+++ makeConnect4MoveHuman.m
@@ -1,5 +1,5 @@
 function [updatedBoard, player, x_loc, y_loc, z_loc] = makeConnect4MoveHuman(board, currentPlayer)
-% places piece in the column that player specifies  
+% Places piece in the column that player specifies  
 % input = board (any size, of all zeros) , current player (symbolized by 1
 % or -1) 
 %
@@ -10,44 +10,48 @@ function [updatedBoard, player, x_loc, y_loc, z_loc] = makeConnect4MoveHuman(boa
 % to use a second time: [updatedBoard, player] = human(updatedBoard, player)
 %   with this you can just keep looping through 
 
-%find the dimensions of the board 
+% store the dimensions of the board 
 dimBoard = size(board);
 numRows = dimBoard(1); 
 numCols = dimBoard(2);
 numSlice = dimBoard(3);
 
-%ask the user which column to drop piece in 
+% ask the user which column to drop piece in 
 col = input('Please enter column number: ');
 slice = input('Please enter in slice number: ');
 
 if col > numCols
     error('Please select a valid column')
-end 
+end
+
 if slice > numSlice
     error('Please select a valid slice')
 end 
 
-%use gravity function 
+% use gravity function (for dropping pieces) 
 for r = 1:numRows
-    %if the spot is not filled and it's the last one, put their piece there
+    
+    % if the spot is not filled and it's the last one, put their piece there
     if board(r, col, slice) == 0 && r == numRows
         board(r,col, slice) = currentPlayer; 
         storeRow = r;
     end 
-    %if the spot isn't filled  and the one right after is, put their piece
-    %there 
+    
+    % if the spot isn't filled  and the one right after is, put their piece there 
     if board(r,col, slice) == 0 && board(r+1, col, slice) ~= 0
         board(r,col, slice) = currentPlayer;
         storeRow = r;
     end 
+    
     %if the spot isn't filled and neither is the one below, break out 
     if board(r, col, slice) == 0 && board(r+1, col, slice) == 0
         board(r,col, slice) = 0; 
         storeRow = r;
     end 
-     
+    
 end 
 
+% setup output values for output matrix
 updatedBoard = board; 
 player = currentPlayer * (-1); 
 x_loc = col;
