30279f819440bc78ce7824459cda3cc2349bd427
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2017-03-14 05:10:44 -0700

    Adds comments for instantWin AI related code

--- instantWin.m
+++ instantWin.m
@@ -1,5 +1,6 @@
 function [col_loc, row_loc, slice_loc] = instantWin(board)
-% outputs the best space
+% instantWin is a 'smart' AI function that attempts to win using a
+% preprogrammed set of strategic moves
 
 % store the dimensions of the board 
 dimBoard = size(board);
@@ -7,11 +8,15 @@ numRows = dimBoard(1);
 numCols = dimBoard(2);
 numSlice = dimBoard(3);
 
-minCol = 1;
-    
+minCol = 1; % the lower bound of the game board
+
+% calculate the middle coordinates of the column and slice dimensions of
+% the game board
 middleCol = round(numCols/2); 
 middleSlice = round(numSlice/2);
     
+    % strategic move function to determine piece position based on
+    % conditions such as where current pieces are located
     if board(numRows, middleCol, middleSlice) == 0
         middleCol = middleCol;
     elseif board(numRows, middleCol, middleSlice) ~= -1
@@ -27,9 +32,11 @@ middleSlice = round(numSlice/2);
     elseif (board(numRows, minCol, middleSlice) == 1) || (board(numRows, minCol, middleSlice) == -1)
         middleCol = numCols;
     end
-    
-col_loc = middleCol; 
-row_loc = numRows;
-slice_loc = middleSlice; 
+
+% store the coordinates x y z coordinates of the piece positions after each
+% AI gameplay decision is made
+col_loc = middleCol;  % x coordinate location
+row_loc = numRows; % y coordinate location
+slice_loc = middleSlice;  % z coordinate location
 
 end 
--- makeConnect4MoveAI.m
+++ makeConnect4MoveAI.m
@@ -18,6 +18,7 @@ numRows = dimBoard(1);
 numCols = dimBoard(2);
 numSlice = dimBoard(3);
 
+% decide whether to use SmartAI:instantWin function during gameplay
 randomPlayDecision = randi(10);
 if rem(randomPlayDecision, 2) == 1 % if its odd
     useSmartAI = 1; % use the instant win AI algorithm
@@ -25,9 +26,12 @@ else  % if its even
     useSmartAI = 0; % let the AI play randomly
 end
 
+% using the SmartAI:instantWin function for AI gameplay
 if (useSmartAI == 1)
-    [col_loc, row_loc, slice_loc] = instantWin(board);
-
+    [col_loc, row_loc, slice_loc] = instantWin(board); % call the instantWin function
+    
+    % ensure that pieces still follow the gravity function when they fall
+    % into the game board
     for r = 1:numRows
         if board(r, col_loc, slice_loc) == 0 && r == numRows
             board(row_loc, col_loc, slice_loc) = currentPlayer;
@@ -43,12 +47,16 @@ if (useSmartAI == 1)
         end
     end
     
+    % store the x y z coordinate locations for the game piece after each
+    % smart AI decision is made
     x_loc = col_loc;
     y_loc = row_loc;
     z_loc = slice_loc;
 
+% not using the SmartAI:instantWin function for AI gameplay and instead
+% using primitive methods
 elseif(useSmartAI == 0)
-    % randomly select column 
+    % randomly select column for primitive AI gameplay 
     col = randi(numCols); 
     slice = randi(numSlice);
 
@@ -71,9 +79,11 @@ elseif(useSmartAI == 0)
         if board(r, col, slice) == 0 && board(r+1, col, slice) == 0
             board(r,col, slice) = 0; 
             storeRow = r;
-        end 
-
-    end 
+        end
+    end
+    
+    % store the x y z coordinate locations for the game piece after each
+    % primitive decision is made
     x_loc = col;
     y_loc = storeRow;
     z_loc = slice;
