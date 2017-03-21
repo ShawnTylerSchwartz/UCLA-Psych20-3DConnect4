5ebeabd2d6ee5990d78ee37ff2ebfb282660d9c9
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2017-03-21 14:26:51 -0700

    Removes old versions of project from master -- now in old master

--- drawblackcircle.m
+++
@@ -1,6 +0,0 @@
-function drawblackcircle(x,y)
-% drawblackcircle plots a black dot at an inputted x,y coordinate location
-
-    plot(x,y,'o','MarkerSize',10,'color','black','MarkerFaceColor','black')
-    
-end
--- drawredcircle.m
+++
@@ -1,6 +0,0 @@
-function drawredcircle(x,y)
-% drawredcircle plots a red dot at an inputted x,y coordinate location
-
-    plot(x,y,'o','MarkerSize',10,'color','red','MarkerFaceColor','red')
-    
-end
